7810c47a75a68081214e62d2d1757262585835d7
Bruno <bruno.freschi2@gmail.com>
2025-12-31 10:29:05 -0300

    add text

---
+++ script.js
@@ -0,0 +1,62 @@
+const content = document.querySelector('.content');
+const btnNew = document.querySelector('.add-note');
+
+
+let dbItems = localStorage.getItem('dbItem')
+  ? JSON.parse(localStorage.getItem('dbItem'))
+  : [];
+
+
+const colors = [
+  "#845EC2",
+  "#008F7A",
+  "#008E9B",
+  "#FFC75F",
+  "#FF8066",
+  "#BA3CAF",
+];
+
+
+const randomColor = () => colors[Math.floor(Math.random() * colors.length)];
+
+btnNew.onclick = () => {
+    addHtml();
+    addEvents();
+};
+
+function loadItems() {
+    content.innerHTML = '';
+    verifyNulls();
+
+    dbItems.forEach((item, i) => {
+        addHtml(item, i);
+    });
+
+    addEvents();
+}
+
+function addHtml(item) {
+    const div = document.createElement('div');
+
+    div.innerHTML = `<div class="item" style="background-color: ${
+        item?.color || randomColor()}">
+        <span class="remove">X</span>
+        <textarea>${item?.text || ""}</textarea>
+    </div>`;
+
+    content.appendChild(div);
+}
+
+function addEvents(item) {
+    const notes = document.querySelectorAll('.item textearea');
+    const remove = document.querySelectorAll('.item .remove');
+
+    notes.forEach((item, i) => {
+        item.oninput = () => {
+            dbItems[i] = {
+                text: item.value,
+                color: dbItems[i]?.color || item.parentElement.style.backgroundColor,
+            }
+        }
+    });
+}
