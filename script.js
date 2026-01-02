979c208ca9622d363c89706067c3610adc2a8108
Bruno <bruno.freschi2@gmail.com>
2026-01-02 17:15:44 -0300

    update

--- script.js
+++ script.js
@@ -6,14 +6,15 @@ let dbItems = localStorage.getItem('dbItems')
     ? JSON.parse(localStorage.getItem('dbItems'))
     : [];
 
-const colors = [
-    '#845EC2',
-    '#008F7A',
-    '#008E9B',
-    '#FFC75F',
-    '#FF8066',
-    '#BA3CAF',
-];
+const colors = [        // const colors = [
+    '#1B9CFC',       //   '#FF7675',
+    '#55EFC4',       //   '#FAB1A0',
+    '#00CEC9',       //   '#FDCB6E',
+    '#74B9FF',       //   '#E84393',
+    '#0984E3',       //   '#E17055',
+    '#6C5CE7',       //   '#D63031',
+];                     // ];
+
 
 const randomColor = () => colors[Math.floor(Math.random() * colors.length)];
 
@@ -38,7 +39,7 @@ function addHTML(item) {
     const div = document.createElement('div');
 
     div.innerHTML = `
-        <div class="item" style="background: ${item?.color || randomGradient()}">
+        <div class="item" style="background: ${item?.color || randomGradient()}; border-radius: 5%; padding: 0 5px;">
           <span class="remove">X</span>
           <textarea>${item?.text || ''}</textarea>
         </div>`;
@@ -49,9 +50,8 @@ function addHTML(item) {
 
 function randomGradient() {
     const color1 = colors[Math.floor(Math.random() * colors.length)];
-    let color2 = colors[Math.floor(Math.random() * colors.length)];
+    const color2 = colors[Math.floor(Math.random() * colors.length)];
 
-    // garante que as cores sejam diferentes
     while (color1 === color2) {
         color2 = colors[Math.floor(Math.random() * colors.length)];
     }
@@ -67,7 +67,7 @@ function addEvents() {
         item.oninput = () => {
             dbItems[i] = {
                 text: item.value,
-                color: dbItems[i]?.color || item.parentElement.style.backgroundColor,
+                color: dbItems[i]?.color || item.parentElement.style.background,
             };
 
             localStorage.setItem('dbItems', JSON.stringify(dbItems));
@@ -76,7 +76,7 @@ function addEvents() {
 
     remove.forEach((item, i) => {
         item.onclick = () => {
-            content.children[i].remove();
+            content.children[i].remove(); 
             if (dbItems[i]) {
                 dbItems.splice(i, 1);
                 localStorage.setItem('dbItems', JSON.stringify(dbItems));
@@ -86,8 +86,8 @@ function addEvents() {
     });
 }
 
-function verifyNulls() {
-    dbItems = dbItems.filter((item) => item);
+function verifyNulls() {    
+    dbItems = dbItems.filter((item) => item && item.text.trim().length > 0);
     localStorage.setItem('dbItems', JSON.stringify(dbItems));
 }
 
